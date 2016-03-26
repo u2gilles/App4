5e18246a69d792e120c7f6034f047d6257f1b06e
u2gilles <u2gilles@gmail.com>
2016-03-26 19:44:35 +0700

    commit 6

--- app/src/main/java/com/example/gilles/app4/MainActivity.java
+++ app/src/main/java/com/example/gilles/app4/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         int i = 3 ;
-        int j = 3;
+        int j = 44;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
